0cb24fe87bbb524acfb05cb1f1214cc1de4052a3
Austin Chopra <austin.chopra@west-mec.org>
2016-08-15 18:00:53 -0700

    worked on code and renamed image

---
+++ game.js
@@ -0,0 +1,18 @@
+var numberOfFaces = 0;
+var score = 0;
+var theLeftSide = document.getElementById("leftSide");
+var count = 0;
+function generateFaces() {
+    img.src="images/smile.png";
+    if (score == 0) {
+        numberOfFaces = 5;
+    }
+    e{
+        //code
+    }
+    while (count <= numberOfFaces) {
+        document.createElement("img");
+        
+    }
+    
+}
