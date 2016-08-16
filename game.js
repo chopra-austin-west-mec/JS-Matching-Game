bbd2a16aa997e47f0695cb70f3f714ba4500099f
Austin Chopra <austin.chopra@west-mec.org>
2016-08-15 18:05:50 -0700

    worked on game.js and added onLoad function to html

--- game.js
+++ game.js
@@ -3,12 +3,13 @@ var score = 0;
 var theLeftSide = document.getElementById("leftSide");
 var count = 0;
 function generateFaces() {
+    
     img.src="images/smile.png";
-    if (score == 0) {
+    if (score <= 0) {
         numberOfFaces = 5;
     }
-    e{
-        //code
+    else if (score == score / 5) {
+        numberOfFaces = score / 5;
     }
     while (count <= numberOfFaces) {
         document.createElement("img");
